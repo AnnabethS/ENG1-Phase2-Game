d4b9a04066d910ba98b11f6d626db300d7a6d5a7
Anna <annabeths111@gmail.com>
2022-04-03 14:46:58 +0100

    correctly run the powerups set of tests

--- tests/src/as/mario/unittesting/powerups/PlayerPowerupTest.java
+++ tests/src/as/mario/unittesting/powerups/PlayerPowerupTest.java
@@ -8,12 +8,27 @@ import tk.shardsoftware.entity.EntityShip;
 import tk.shardsoftware.screens.GameScreen;
 import tk.shardsoftware.util.Difficulty;
 import tk.shardsoftware.util.PowerupType;
+import tk.shardsoftware.util.ResourceUtil;
+
+import org.junit.runner.RunWith;
+import org.junit.BeforeClass;
+import as.mario.unittesting.GdxTestRunner;
+
+import com.badlogic.gdx.assets.AssetManager;
 
 /*
   @author Anna Singleton
  */
+@RunWith (GdxTestRunner.class)
 public class PlayerPowerupTest
 {
+	@BeforeClass
+	public static void entityTestInit()
+	{
+		AssetManager a = new AssetManager();
+		ResourceUtil.init(a);
+	}
+
 	@Test
 	public void testPowerup()
 	{
